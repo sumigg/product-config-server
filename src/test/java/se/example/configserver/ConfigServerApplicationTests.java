2a00ca5c119efbc2c374afc41d9e070b09cf6937
sumigg <with.sumitaggarwal@gmail.com>
2025-09-18 06:09:10 +0000

    add chapter12 config-server code

---
+++ src/test/java/se/example/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.example.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
